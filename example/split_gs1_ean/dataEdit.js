b2eb059b71e28c0c3a6363aeeaf9e0afda1c7a06
hjgode <hjgode@gmail.com>
2019-05-06 15:28:26 +0200

    added new example

---
+++ example/split_gs1_ean/dataEdit.js
@@ -0,0 +1,31 @@
+function myStartsWith(inStr, lookfor) {
+   if(inStr.length < lookfor.length) {
+	return false;
+  }
+  front=inStr.substring(0,lookfor.length);
+  if(front == lookfor) {
+    return true;
+  }
+  return false;
+}
+
+   var testStr = [ "020840159020016215200103370088", "10S010330201", "00984296833021002023" ];
+
+function dataEdit(inStr, sAimID) { 
+ 
+ var vOut=inStr;
+  if( myStartsWith(inStr, "02") ){
+    vOut = inStr.slice(2, 16) + " " + inStr.slice(18, 24) +  " " + inStr.slice (26);
+    return vOut;
+  }
+  if(myStartsWith(inStr, "10")) {
+    vOut=inStr.slice(2);
+    return vOut;
+  }
+  if(myStartsWith(inStr, "00")) {
+    vOut=inStr.slice(2);
+    return vOut;
+  }
+
+  return vOut;
+}
